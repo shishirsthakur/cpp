6b139248572abb7ea6f4f0ec7e89e1eee39cdff0
shishirsthakur <shishirth4kur@gmail.com>
2025-06-30 21:05:22 +0530

    Add luvcpp folder and move areaofrect.cpp into it

---
+++ luvcpp/areaofrect.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+	cout << 7 /2 * 3 << endl;
+	cout << 3*7/2 <<endl;
+	int a = 100000;
+	int b = 100000;
+	long int c = a* 1LL * b;
+	cout << c << endl;
+	double x = 100000;
+	double y = 100000;
+	double z = a* 1LL * b;
+	cout << z << endl;
+	double p = 100000;
+	double q = 100000;
+	double r = a* 1LL * b;
+	cout << fixed<<r << endl;
+	double j = 100000;
+	double k = 100000;
+	double l = a* 1LL * b;
+	cout << fixed<< setprecision(0) <<r << endl;
+}
