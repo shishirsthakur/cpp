bd269f1b1ef1179d6c4c1584d8bc57871f35a971
shishirsthakur <shishirth4kur@gmail.com>
2025-07-01 21:02:05 +0530

    string and getline functions

---
+++ luvcpp/stringgetline.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	string str1, str2;
+   str1 = "Hello";
+   cout << str1.size()<<endl;
+   cout << str1[0]<< endl;
+
+getline(cin, str2); //inputs the whole line
+	cout <<str2;
+	return 0;
+}
