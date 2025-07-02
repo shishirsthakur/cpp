0b02174e77fab12d54d12751ed4d3ec4cbad5af5
shishirsthakur <shishirth4kur@gmail.com>
2025-07-02 10:25:47 +0530

    swap two numbers by using references

---
+++ luvcpp/swapnumber.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int &a, int &b){
+	int temp=a;
+	a=b;
+	b=temp;
+}
+
+
+int main(){
+	int a =3, b=5;
+	cout << a << " "<< b << endl;
+	swap(a, b);
+	cout << a<< " " << b <<endl;
+	
+	return 0;
+}
