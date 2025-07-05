30e1f033c27755530dd53d575c157a477d8e7825
shishirsthakur <shishirth4kur@gmail.com>
2025-07-05 22:49:33 +0530

    time complexity

---
+++ luvcpp/timecomp.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+// int main(){
+// 	int n;
+// 	cin >> n;
+// 	 int ct = 0;
+// 	 while(n>0){
+// 	 	n = n/2;
+// 	 	ct++;
+// 	 }
+	
+// }
+// // o(1) + o(1) = 0(log(n)) = o(log(n))
+// int main(){
+// 	int n;
+// 	cin >> n;
+// 	for(int i = 0; i < n; ++i){
+// 		for(int j = 0; j< i; ++j){
+
+// 		}
+// 	}
+// }
+// // o(n^2)
