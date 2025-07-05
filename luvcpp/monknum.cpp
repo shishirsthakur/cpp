1b86e6bbf3f651bb6a4c771b3b826a533d83001a
shishirsthakur <shishirth4kur@gmail.com>
2025-07-05 10:40:08 +0530

    question 7

---
+++ luvcpp/monknum.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+char upper(char c){
+	return 'A'+(c-'a');
+}
+char lower(char c){
+	return 'a'+(c-'A');
+}
+int main(){
+	string s;
+	cin>> s;
+	for(int i=0; i<s.size(); ++i){
+		if(s[i]>='a' && s[i] <= 'z'){
+			s[i] = upper(s[i]);
+		}
+		else{
+			s[i]= lower(s[i]);
+		}
+	} int result =0;
+	for(int i=0; i<s.size(); ++i){
+		if(s[i]>='a' && s[i] <= 'z'){
+			result = result - s[i];
+		}
+		else{
+			result = result + s[i];
+		}
+	}
+	if(result<0){
+		result = result *-1;
+	}
+	int flag = 0;
+	for (int i= 2; i<result; ++i){
+		if(result%i==0){
+			flag = 1;
+			break;
+		}
+	}
+	if(flag==0){
+		cout << 1;
+	}else cout << 0;
+}
