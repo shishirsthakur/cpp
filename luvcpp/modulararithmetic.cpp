9c7aae375bf2b87ac2795aed0c30f2db033251cc
shishirsthakur <shishirth4kur@gmail.com>
2025-07-06 09:44:33 +0530

    Modular Arithmetic

---
+++ luvcpp/modulararithmetic.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cin >> n;
+long long fact = 1;
+for(int i = 2; i<=n; ++i){
+	fact = fact*i % M;
+}
+	
+	
+}
+
+// 1. (a + b) % M = ((a%M) + (b%M)) % M;
+// 2. (a * b) % M = ((a%M) * (b%M)) % M;
+// 3. (a * b) % M = ((a%M) - (b%M)) % M;
+// 4. (a/b % M)   = ((a%M) * (b^-1 % M
+// print answer modulo M reduces overflow
+// break down the value according to the formula
