1cb997b29ea7a6ab9d062eede7933f7d32aa35db
shishirsthakur <shishirth4kur@gmail.com>
2025-07-10 23:09:11 +0530

    Factorials using recursion

---
+++ luvcpp/recursion.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+//print factorial on N
+
+int fact(int n){
+	if(n==0) return 1;
+	return fact(n-1)*n;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	cout << fact(n);
+	
+	
+}
+// No of function calls
+// Compleexity of fxn is required for TC
