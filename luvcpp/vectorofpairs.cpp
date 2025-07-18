98fdc79b2d9df1bf2cdfe0d95d69c64bd38476aa
shishirsthakur <shishirth4kur@gmail.com>
2025-07-18 22:47:22 +0530

    Making a vector of pairs

---
+++ luvcpp/vectorofpairs.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printvect(vector<pair<int,int> > &v){
+	cout << "size: " << v.size() << endl;
+	for(int i = 0; i < v.size(); ++i){
+		cout << v[i].first <<" " << v[i].second <<endl;
+	}
+	cout << endl;
+}
+int main(){
+	vector<pair<int,int> > v;
+	int n;
+	cin >> n;
+	for(int i = 0; i < n; ++i){
+		int x, y;
+		cin >> x >> y;
+		v.push_back({x,y}); //curly brackets used to make pairs
+		}                   //you can also do (make_pair(x,y))
+		printvect(v);
+	
+}
