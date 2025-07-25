df8dbce9a7b2271021b870edabd39670d361c3cc
shishirsthakur <shishirth4kur@gmail.com>
2025-07-25 22:04:20 +0530

    Question on sum of digits

---
+++ Questions/Codechef/sumdigits.cpp
@@ -0,0 +1,22 @@
+// https://www.codechef.com/problems/FLOW006?tab=statement
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int n;
+		cin >> n;
+		int sum = 0;
+         while(n>0){
+			int rem = n%10;
+			sum = sum + rem;
+			n = n/10;
+
+		}
+		cout << sum << endl;
+
+	}	
+}
