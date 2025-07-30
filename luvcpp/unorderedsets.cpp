e28789c7e328506c95c1ca841421c99f6ef89769
shishirsthakur <shishirth4kur@gmail.com>
2025-07-30 23:10:04 +0530

    Unordered Sets

---
+++ luvcpp/unorderedsets.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(unordered_set<string> &s){
+	for(string value: s){
+		cout << valueb << endl;
+	}
+}
+int main(){
+	unordered_set<string> s;
+	s.insert("abc");
+	s.insert("zsdf");
+	s.insert("bcd");
+	s.insert("abc");
+	auto it = s.find("abc");
+	if(it != s.end()){
+		s.erase(it);
+	}
+	print(s);
+	
+
+}
