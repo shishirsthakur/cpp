65e33e1d7f14bfdd73acbbc4dbf359cb5dc3db2a
shishirsthakur <shishirth4kur@gmail.com>
2025-09-04 22:11:52 +0530

    easy qs

---
+++ Questions/TLE/L1/F. Digitssummation.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+ios::sync_with_stdio(0);
+cin.tie(0);
+long long m, n;
+cin >> m >> n;
+int a, b;
+a = m%10;
+b = n%10;
+cout << a + b;
+
+
+}
