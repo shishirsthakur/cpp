7e780f9b53b9332722e60b1919c1ef170d6e1a19
shishirsthakur <shishirth4kur@gmail.com>
2025-09-30 21:27:55 +0530

    Contest 1

---
+++ Questions/Codeforces/iutps/Sheet-1/contest 1/A.Winter.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+ios::sync_with_stdio(0);
+cin.tie(0);
+float a, b;
+cin >> a >> b;
+float c = a/100;
+
+float d = 1 - c;
+
+float e = b/d;
+cout << e;
+
+
+}
