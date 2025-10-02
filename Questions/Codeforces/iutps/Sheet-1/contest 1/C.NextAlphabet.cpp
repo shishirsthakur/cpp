245f18463ac61dd3d1dda542d36c7bb659bae095
shishirsthakur <shishirth4kur@gmail.com>
2025-10-02 07:15:36 +0530

    Contest 1

---
+++ Questions/Codeforces/iutps/Sheet-1/contest 1/C.NextAlphabet.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+ios::sync_with_stdio(0);
+cin.tie(0);
+char c;
+cin >> c;
+if(c=='z'){
+	cout << "a";
+}
+else {
+	char d = c+1;
+cout << d;
+}
+}
