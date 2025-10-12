4d49f55e2b346be798fd6e29af4a4c14d75e10df
shishirsthakur <shishirth4kur@gmail.com>
2025-10-12 12:31:56 +0530

    Sheet 2

---
+++ Questions/Codeforces/iutps/Sheet-2/D.FixPass.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+      while(true){
+        int n;
+        cin  >> n;
+        if(n==1999){
+            cout  << "Correct" <<endl;
+            break;
+        }
+        else cout << "Wrong" <<endl;
+      }
+}
