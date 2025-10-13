b570092bf5d56766681ab30d784bd41e3b133bd0
shishirsthakur <shishirth4kur@gmail.com>
2025-10-13 20:52:24 +0530

    Sheet 2

---
+++ Questions/Codeforces/iutps/Sheet-2/K.Divisors.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for(int i = 1; i <=n; i++){
+        if(n%i==0){
+            cout << i << '\n';
+        }
+    }
+    
+}
