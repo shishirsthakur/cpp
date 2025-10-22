ffa24ef7102e33a36287cfa08caad8d6ac7b3680
shishirsthakur <shishirth4kur@gmail.com>
2025-10-22 23:14:57 +0530

    Sheet 3

---
+++ Questions/Codeforces/iutps/Sheet-3/F.Reverse.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    long long int n;
+    cin >> n;
+    long long int arr[n];
+    for(int i =0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    for(int i = n-1 ; i >= 0; i--){
+        cout << arr[i] << " ";
+    }
+    
+
+}
