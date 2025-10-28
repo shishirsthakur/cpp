3d965c7bd41d4ad4e3c9080cf77c3a8fe289d39b
shishirsthakur <shishirth4kur@gmail.com>
2025-10-28 23:00:52 +0530

    Div 4

---
+++ Questions/Codeforces/Contests/Div.4(28.10.25)/A.Square.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+int a, b , c , d;
+cin >> a >> b >> c >> d;
+if(a == b  && b == c && c == d){
+    cout << "YES\n";
+}
+else cout << "NO\n";
+
+
+    }
+}
