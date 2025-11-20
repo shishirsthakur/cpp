4a810e47b296f2052d9e4c9d06c778f93ecfcccd
shishirsthakur <shishirth4kur@gmail.com>
2025-11-20 20:29:02 +0530

    Problem A

---
+++ Questions/Codeforces/Contests/Div.3(20.11.25)/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution(){
+   long long int n;
+   cin >> n;
+   long long count2 = 0, count4 = 0;
+   long long total =0;
+   if(n%2==0){
+    cout << (n/4) + 1 << "\n";
+   }
+   else cout << '0' << "\n";
+}
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t; 
+    cin >> t;
+    while(t--) 
+    solution();
+}
