364c2d39da7e93f1dcc72e5a83ad15e2363bffbf
shishirsthakur <shishirth4kur@gmail.com>
2025-12-12 23:44:50 +0530

    type shi

---
+++ Questions/TLE/CP-31/900/11.NotDividing.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    long long n;
+        cin >> n;
+        long long arr[n];
+        for (int i = 0; i < n; i++){
+            cin >> arr[i];
+        }
+        for (int i = 0; i < n; i++){
+         if (arr[i] == 1){
+                     arr[i]++;
+                 }
+    }
+    for (int i = 0; i < n - 1; i++){
+        if (arr[i + 1] % arr[i] == 0){
+            arr[i + 1]++;
+        }
+    }
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+        cout << "\n";   
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+}
