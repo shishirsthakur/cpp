0421ddd416315958414e2460a2c0c542d6e1c85a
shishirsthakur <shishirth4kur@gmail.com>
2025-12-13 21:14:59 +0530

    i will kms soon

---
+++ Questions/Codeforces/Practice/Div4/A/TrippiTroppi.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+   string a, b, c;
+   cin >> a >> b >> c;
+   cout << a[0] << b[0]  << c[0] << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+}
