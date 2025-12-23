c78e1d6a713834019d60d52ca72ab2fd90e05632
shishirsthakur <shishirth4kur@gmail.com>
2025-12-23 23:30:00 +0530

    type shi

---
+++ Questions/Codeforces/Contests/Div3(23.12.25)/A.BlackslexPassword.cpp
@@ -0,0 +1,20 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    void solution() {
+        long long k, x;
+        cin >> k >> x;
+        long long soln = k*x+1;
+        cout << soln << "\n";
+    }
+     
+    int main() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        
+        int t;
+        cin >> t;
+        while (t--) {
+            solution();
+        }
+    }
