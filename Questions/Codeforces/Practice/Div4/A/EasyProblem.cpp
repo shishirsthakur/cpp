2df9ddf3a93c7766a7faccc58dad25a76e1405d8
shishirsthakur <shishirth4kur@gmail.com>
2025-12-30 23:30:00 +0530

    type shi

---
+++ Questions/Codeforces/Practice/Div4/A/EasyProblem.cpp
@@ -0,0 +1,19 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    void solution() {
+        long long n;
+        cin >> n;
+        cout << n-1 << "\n";
+    }
+     
+    int main() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        
+        int t;
+        cin >> t;
+        while (t--) {
+            solution();
+        }
+    }
