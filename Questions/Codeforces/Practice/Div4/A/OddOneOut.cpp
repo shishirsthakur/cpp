f9dd9135aa7877d5b8cab74345644e3b6fc28b11
shishirsthakur <shishirth4kur@gmail.com>
2026-01-01 23:30:00 +0530

    its da first of the year

---
+++ Questions/Codeforces/Practice/Div4/A/OddOneOut.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+   long long a, b , c;
+   cin >> a >> b >> c;
+   if(a == b){
+    cout << c << "\n";
+   }
+   else if(b == c){
+    cout << a << "\n";
+   }
+   else cout << b << "\n";
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+}
