18d22259973a01749ac9cb8a001c10a5ddee3e37
shishirsthakur <shishirth4kur@gmail.com>
2026-01-04 22:32:14 +0530

    4rth of da month

---
+++ Questions/Codeforces/Practice/Div4/B/BobrittoBandito.cpp
@@ -0,0 +1,31 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    void solution() {
+        int n, m, l, r;
+        cin >> n >> m >> l >> r;
+        
+        int lm = -m / 2;
+        int rm = lm + m;
+        
+        if (rm > r) {
+            rm = r;
+            lm = r - m;
+        } else if (lm < l) {
+            lm = l;
+            rm = l + m;
+        }
+        
+        cout << lm << " " << rm << "\n";
+    }
+     
+    int main() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        
+        int t;
+        cin >> t;
+        while (t--) {
+            solution();
+        }
+    }
