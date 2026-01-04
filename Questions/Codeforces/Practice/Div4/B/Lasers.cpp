9a9580aae8fcb6a6ed3c44741bca7d73c694e873
shishirsthakur <shishirth4kur@gmail.com>
2026-01-04 22:18:15 +0530

    4rth of da month

---
+++ Questions/Codeforces/Practice/Div4/B/Lasers.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    long long a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >>f;
+    cout << a+b << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+}
