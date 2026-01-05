e38354f8bc9ef89784e90437bb2cd46592314115
shishirsthakur <shishirth4kur@gmail.com>
2026-01-05 16:35:15 +0530

    5/15

---
+++ Questions/Codeforces/Practice/Div4/B/MaximumMultipleSum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    int n;
+    cin >> n;
+    if(n == 3){
+        cout << '3' << "\n";
+    }
+    else cout << '2' << "\n";
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+}
