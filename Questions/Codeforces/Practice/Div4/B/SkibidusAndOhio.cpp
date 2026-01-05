8a1e2beef340a9273e0eb09d15bcf75fff605993
shishirsthakur <shishirth4kur@gmail.com>
2026-01-05 12:25:55 +0530

    1/15

---
+++ Questions/Codeforces/Practice/Div4/B/SkibidusAndOhio.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    string s;
+    cin >> s;
+    bool f = false;
+    for(int i = 0; i < (int)s.size()-1; i++){
+        if(s[i] ==s[i+1]){
+            f = true;
+        }
+
+    }
+    if(f){
+        cout << "1" << "\n";
+    }
+    else cout << (int)s.size() << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+}
