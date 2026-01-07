c66ae5f06f8c5dd7970dce1b7d97a98552285eb5
shishirsthakur <shishirth4kur@gmail.com>
2026-01-07 19:30:38 +0530

    2/10

---
+++ Questions/TLE/L1/NT/OddDivisor.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    long long int n;
+    cin >> n;
+    bool f = false;
+    while(n > 1){
+        if(n%2 == 0){
+            n/=2;
+        }
+        else{
+            f = true;
+            
+         break;
+}
+    }
+    if(f){
+        cout << "YES" << "\n";
+    }
+    else cout << "NO" << "\n";
+    }
+
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+}
