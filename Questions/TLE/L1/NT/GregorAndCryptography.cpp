b6306557646174484f0620de2d33fbe8a660e655
shishirsthakur <shishirth4kur@gmail.com>
2026-01-07 19:16:29 +0530

    1/10

---
+++ Questions/TLE/L1/NT/GregorAndCryptography.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    int n;
+    cin >> n;
+    cout << '2' << " " << n-1 << "\n";
+    }
+
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+}
