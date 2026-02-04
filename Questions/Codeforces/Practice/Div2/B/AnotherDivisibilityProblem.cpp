91e105b186ea6b183f88dfd037e48a21cafeb2ea
shishirsthakur <shishirth4kur@gmail.com>
2026-02-04 22:42:58 +0530

    type shi

---
+++ Questions/Codeforces/Practice/Div2/B/AnotherDivisibilityProblem.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+     long long n;
+    cin >> n;
+    long long m = 2*n;
+    cout <<m << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long long t;
+    cin >> t;
+    while(t--){
+    solve();
+}
+}
