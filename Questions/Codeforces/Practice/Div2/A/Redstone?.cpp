a7c112517da65e26fe7cd85f7ab9fbda7dc44b91
shishirsthakur <shishirth4kur@gmail.com>
2026-02-13 23:06:52 +0530

    type shi

---
+++ Questions/Codeforces/Practice/Div2/A/Redstone?.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++){
+     cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1]) {
+            cout << "YES\n";
+            return;
+        }
+    }
+    cout << "NO\n";
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--){
+     solve();
+}
+}
